51606b04d8e42eaf044d0a7f6fa0c9425b577a19
tch <taochenhao@qq.com>
2019-05-06 11:37:42 +0800

    提交

--- utils/config.js
+++ utils/config.js
@@ -1,5 +1,5 @@
-//var baseUrl ="http://117.51.154.173/zijida/";
-var baseUrl = "http://127.0.0.1/";
+var baseUrl ="http://117.51.154.173/zijida/";
+//var baseUrl = "http://127.0.0.1/";
 var appId ="wxfdd4045cf84887fa";
 var appSecret = "fb1abace6fe9e321893e8daa47166846";
 
